50219f48b5f8899a9cf5d0b5882a51ffae6150e3
Deacon <deaconpoon@gmail.com>
2020-09-16 00:22:55 -0700

    Add search feature to menu

--- client/src/component/Menu.js
+++ client/src/component/Menu.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { CSSTransition } from "react-transition-group";
 import cross from "../asset/cross.svg";
+import SideMenu from "./SideMenu";
+import search from "../asset/search.svg";
 
 const Menu = ({ menuOpen, setMenuOpen }) => {
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [expand, setExpand] = useState(false);
+
+  const closeExpand = () => {
+    document.querySelector(".menu__body__shop--items").classList.add("hide");
+    document
+      .querySelector(".menu__body__shop--arrow--img")
+      .classList.add("flip");
+    setExpand(!expand);
+  };
+  const openExpand = () => {
+    document.querySelector(".menu__body__shop--items").classList.remove("hide");
+    document
+      .querySelector(".menu__body__shop--arrow--img")
+      .classList.remove("flip");
+    setExpand(!expand);
+  };
+  const handleExpand = (e) => {
+    e.preventDefault();
+
+    return expand ? openExpand() : closeExpand();
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
       <CSSTransition classNames="fade" in={menuOpen} appear={menuOpen}>
@@ -25,19 +54,42 @@ const Menu = ({ menuOpen, setMenuOpen }) => {
           </header>
           <div className="menu__body__wrapper">
             <div className="menu__body">
-              <div className="menu__body__container">
-                <h2 className="menu__body__search">Search</h2>
-                <div className="menu__body__search--arrow">
+              <div className="menu__body__search menu__items">
+                <form className="menu__body__search--bar">
+                  <input
+                    className="menu__body__search--input"
+                    type="text"
+                    name="searchKeyword"
+                    id="search-input"
+                    placeholder="Search"
+                    onChange={(e) => setSearchKeyword(e.target.value)}
+                  ></input>
+                </form>
+                <span className="menu__body__search--icon">
                   <img
-                    alt="arrow"
-                    src={require("../asset/arrow.svg")}
-                    className="menu__body__search--arrow--img"
+                    alt="search"
+                    src={search}
+                    className="menu__body__search--img"
                   ></img>
-                </div>
+                </span>
               </div>
-              <h2>Shop</h2>
-              <h2>About</h2>
-              <h2>Contact</h2>
+              <div className="menu__items">
+                <div className="menu__body__shop" onClick={handleExpand}>
+                  <h2 className="menu__body__shop--title">Shop</h2>
+                  <div className="menu__body__shop--arrow">
+                    <img
+                      alt="arrow"
+                      src={require("../asset/arrow.svg")}
+                      className="menu__body__shop--arrow--img flip"
+                    ></img>
+                  </div>
+                </div>
+                <ul className="menu__body__shop--items hide">
+                  <SideMenu></SideMenu>
+                </ul>
+              </div>
+              <h2 className="menu__items">About</h2>
+              <h2 className="menu__items">Contact</h2>
             </div>
           </div>
         </session>
