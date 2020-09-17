45e3bc608c51ac08b00f3e6a3219d103a68d98f9
Deacon <deaconpoon@gmail.com>
2020-09-17 14:02:38 -0700

    Add click to scroll feature

--- client/src/component/Hero.js
+++ client/src/component/Hero.js
@@ -1,29 +1,51 @@
-import React from "react";
+import React, { Fragment, useRef } from "react";
 import arrow from "../asset/next.svg";
+import arrowWhite from "../asset/arrow_white.svg";
 import { useHistory } from "react-router-dom";
 
 const Hero = () => {
   const history = useHistory();
-  return (
-    <div className="hero">
-      <div className="hero__content">
-        <p className="hero__content__title">
-          topical paradise in the urban jungle.
-        </p>
-        <div
-          onClick={() => history.push("/products")}
-          className="hero__content__collection"
-        >
-          <div className="hero__content__collection--text">
-            see our collection
-          </div>
+  const scrollToRef = (ref) =>
+    window.scrollTo({
+      top: ref.current.offsetTop,
+      behavior: "smooth",
+    });
+  const myRef = useRef(null);
 
-          <div className="hero__content__collection--container">
-            <img className="hero__content__collection--img" src={arrow}></img>
+  return (
+    <Fragment>
+      <div className="hero">
+        <div>
+          <div className="hero__content">
+            <p className="hero__content__title">
+              topical paradise in the urban jungle.
+            </p>
+            <div
+              onClick={() => history.push("/products")}
+              className="hero__content__collection"
+            >
+              <div className="hero__content__collection--text">
+                see our collection
+              </div>
+
+              <div className="hero__content__collection--container">
+                <img
+                  className="hero__content__collection--img"
+                  src={arrow}
+                ></img>
+              </div>
+            </div>
           </div>
         </div>
+        <button
+          onClick={() => scrollToRef(myRef)}
+          className="hero__content__arrow"
+        >
+          <img className="hero__content__arrow--img" src={arrowWhite}></img>
+        </button>
       </div>
-    </div>
+      <div ref={myRef}></div>
+    </Fragment>
   );
 };
 
