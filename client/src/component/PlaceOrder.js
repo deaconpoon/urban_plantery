c3f16f6f481ee18cfe7f42c303d7fb1cbd6e4b21
Deacon <deaconpoon@gmail.com>
2020-09-17 22:50:19 -0700

    Styling for place order page

--- client/src/component/PlaceOrder.js
+++ client/src/component/PlaceOrder.js
@@ -46,40 +46,48 @@ function PlaceOrder(props) {
   return (
     <div>
       <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
-      <div className="placeorder">
+      <body className="placeorder section">
         <div className="placeorder-info">
           <div>
-            <h3>Shipping</h3>
+            <h3 className="form__title">Shipping</h3>
             <div>
               {cart.shipping.address}, {cart.shipping.city},
               {cart.shipping.postalCode}, {cart.shipping.country},
             </div>
           </div>
           <div>
-            <h3>Payment</h3>
+            <h3 className="form__title">Payment</h3>
             <div>Payment Method: {cart.payment.paymentMethod}</div>
           </div>
           <div>
             <ul className="cart-list-container">
               <li>
-                <h3>Shopping Cart</h3>
-                <div>Price</div>
+                <h3 className="form__title">Shopping Cart</h3>
               </li>
               {cartItems.length === 0 ? (
                 <div>Cart is empty</div>
               ) : (
                 cartItems.map((item) => (
-                  <li>
-                    <div className="cart-image">
-                      <img src={item.image} alt="product" />
+                  <li className="cart-item--container">
+                    <div>
+                      <img
+                        className="cart--image"
+                        src={item.image}
+                        alt="product"
+                      />
                     </div>
-                    <div className="cart-name">
-                      <div>
-                        <Link to={"/product/" + item.product}>{item.name}</Link>
+                    <div className="cart-item--container--2">
+                      <div className="cart--name">
+                        <div>
+                          <Link to={"/product/" + item.product}>
+                            {item.name}
+                          </Link>
+                        </div>
+                        <div>Quantity: {item.quantity}</div>
+
+                        <div className="cart-price">${item.price}</div>
                       </div>
-                      <div>Qty: {item.quantity}</div>
                     </div>
-                    <div className="cart-price">${item.price}</div>
                   </li>
                 ))
               )}
@@ -89,15 +97,7 @@ function PlaceOrder(props) {
         <div className="placeorder-action">
           <ul>
             <li>
-              <button
-                className="button primary full-width"
-                onClick={placeOrderHandler}
-              >
-                Place Order
-              </button>
-            </li>
-            <li>
-              <h3>Order Summary</h3>
+              <h3 className="form__title">Order Summary</h3>
             </li>
             <li>
               <div>Items</div>
@@ -111,13 +111,18 @@ function PlaceOrder(props) {
               <div>Tax</div>
               <div>${taxPrice}</div>
             </li>
-            <li>
+            <li className="placeorder-action--total">
               <div>Order Total</div>
               <div>${totalPrice}</div>
             </li>
+            <li className="">
+              <button className="form__signIn" onClick={placeOrderHandler}>
+                Place Order
+              </button>
+            </li>
           </ul>
         </div>
-      </div>
+      </body>
     </div>
   );
 }
