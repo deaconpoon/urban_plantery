6513d3cf12c138952589dec0503db8fb8d1b66b1
Deacon <deaconpoon@gmail.com>
2020-09-17 17:03:42 -0700

    Styling and add link to footer

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -1,7 +1,14 @@
 import React from "react";
 import logo_black from "../asset/logo_black.svg";
+import { useHistory } from "react-router";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push("/");
+  };
   return (
     <footer className="section">
       <div className="footer">
@@ -11,13 +18,22 @@ const Footer = () => {
               alt="logo"
               className="footer__logo--image"
               src={logo_black}
+              onClick={handleClick}
             ></img>
           </div>
           <div className="footer__links">
-            <div className="footer__link footer__links--1">Shop</div>
-            <div className="footer__link  footer__links--2">About</div>
-            <div className="footer__link  footer__links--3">Contact</div>
-            <div className="footer__link  footer__links--4">Home</div>
+            <Link to="/" className="footer__link footer__links--1">
+              Home
+            </Link>
+            <Link to="/about" className="footer__link  footer__links--2">
+              About
+            </Link>
+            <Link to="/products" className="footer__link  footer__links--3">
+              Shop
+            </Link>
+            <Link to="/contact" className="footer__link  footer__links--4">
+              Contact
+            </Link>
           </div>
         </div>
         <div className="footer__copyright">© 2020 Urban Plantery</div>
